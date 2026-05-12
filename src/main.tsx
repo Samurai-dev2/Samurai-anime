778120bc8b13372e1a85f561ad5a455aa2165517
Samurai.dev <ibrahim.farhat@stu.amly.us>
2026-05-12 14:01:00 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+if (!publishableKey) {
+  throw new Error('Missing Clerk publishable key');
+}
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
